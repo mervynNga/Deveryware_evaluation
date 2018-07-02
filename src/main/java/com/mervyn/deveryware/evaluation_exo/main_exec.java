78037f760de8f4050262ee7d026e77c8610e184a
mervyn <merngamini@yahoo.fr>
2018-07-02 14:34:41 +0200

    -initial commit

---
+++ src/main/java/com/mervyn/deveryware/evaluation_exo/main_exec.java
@@ -0,0 +1,9 @@
+package com.mervyn.deveryware.evaluation_exo;
+
+public class main_exec {
+	
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

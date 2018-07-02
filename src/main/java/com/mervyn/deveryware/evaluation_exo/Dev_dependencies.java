1af96a6cf467cdd71471a27c1f33a136b3e9e457
mervyn <merngamini@yahoo.fr>
2018-07-02 14:51:46 +0200

    - creation of the object to save a Yaml's dev_dependencies

---
+++ src/main/java/com/mervyn/deveryware/evaluation_exo/Dev_dependencies.java
@@ -0,0 +1,17 @@
+package com.mervyn.deveryware.evaluation_exo;
+
+public class Dev_dependencies {
+	String test = "any";
+
+	public Dev_dependencies() {
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+}
